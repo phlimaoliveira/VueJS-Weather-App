0649588cff183cf0c9291e062c4b3880414e5cb3
Paulo Henrique <ph.lima.tecnico@gmail.com>
2021-11-12 18:04:39 -0300

    Checking if Location inputed by user is valid, if not informed the user

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <div id="app" :class="typeof weather.main != 'undefined' && weather.main.temp > 16 ? 'warm': ''">
+  <div id="app" :class="typeof weather.main != 'undefined' && 
+                        weather.main.temp > 16 ? 'warm': ''">
     <main>
       <div class="search-box">
         <input type="text" 
@@ -7,6 +8,12 @@
                placeholder="Search..."
                v-model="query"
                @keypress="fetchWeather" />
+      </div>
+
+      <div class="weather-wrap" v-if="weather.cod != 200">
+        <div class="location-box">
+          <div class="location">Oooops, City not Founded</div>          
+        </div>
       </div>
 
       <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
@@ -43,9 +50,10 @@
       fetchWeather (e) {
         if(e.key == "Enter") {
           fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
-            .then(res => {
-              return res.json();
-            }).then(this.setResults);
+            .then(res => {              
+              return res.json();              
+            }).then(this.setResults)
+            .catch((err) => console.log(err));
         }
       },
       setResults(results) {
@@ -63,6 +71,13 @@
 
         return `${day} ${date} ${month} ${year}`;
       },
+    },
+    beforeMount() {
+      let query = 'Jataí, BR'
+      fetch(`${this.url_base}weather?q=${query}&units=metric&APPID=${this.api_key}`)
+        .then(res => {
+          return res.json();
+        }).then(this.setResults);
     }
   }
 </script>
